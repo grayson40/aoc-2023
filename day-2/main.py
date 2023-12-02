eacd2438e7bc29f0f4b69637b75a40b23cce67ae
grayson40 <graysoncrozier40@gmail.com>
2023-12-02 13:43:47 -0500

    day 2 part 2 solution

--- day-2/main.py
+++ day-2/main.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 MAX_COLORS = {'red': 12, 'green': 13, 'blue': 14}
 
 
@@ -40,4 +42,43 @@ def part_1():
         return total
 
 
+def part_2():
+    with open('input.txt', 'r') as f:
+        powers = []
+        for line in f.readlines():
+            # Create a dictionary of the numbers for each color
+            colors = {'red': [], 'green': [], 'blue': []}
+
+            # Get the game ID's start and end index
+            num_start_index = line.find(' ') + 1
+            num_end_index = line.find(':', num_start_index)
+
+            # Get the number of red, green, and blue in each set, separated by ;
+            sets = line[num_end_index + 1:].split(';')
+            for set in sets:
+                set = set.strip()
+                if set == '':
+                    continue
+
+                # Get the number and color of each subset, separated by ,
+                subsets = set.split(', ')
+                for subset in subsets:
+                    num = int(subset[0: subset.find(' ')])
+                    color = subset[subset.find(' ') + 1:]
+                    # Add number to color list
+                    colors[color].append(num)
+
+            # Get the minimum number of each color
+            mins = []
+            for color in colors:
+                mins.append(max(colors[color]))
+
+            # Add the product of the minimums to the powers list
+            powers.append(reduce(lambda x, y: x * y, mins))
+
+        # Return the sum of the powers
+        return sum(powers)
+
+
 print(part_1())
+print(part_2())
